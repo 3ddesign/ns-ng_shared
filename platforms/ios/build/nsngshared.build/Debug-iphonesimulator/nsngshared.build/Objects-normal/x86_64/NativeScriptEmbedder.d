aa455da8ad97a6b6620072122e0e92ad5f534ae8
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-08-10 08:43:16 +0300

    remove platforms

--- platforms/ios/build/nsngshared.build/Debug-iphonesimulator/nsngshared.build/Objects-normal/x86_64/NativeScriptEmbedder.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/dmitry/Documents/Projects/ns-ng_shared/platforms/ios/nsngshared/nsngshared-Prefix.pch \
-  /Users/dmitry/Documents/Projects/ns-ng_shared/node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.m \
-  /Users/dmitry/Documents/Projects/ns-ng_shared/node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.h
