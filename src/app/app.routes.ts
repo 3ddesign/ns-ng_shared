898e4420ac9859381df9c07b0d3cd33bde5f6367
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-08-30 07:45:31 +0300

    replace challenges, route changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-      path: '',
-      redirectTo: '/auth',
-      pathMatch: 'full',
+    path: '',
+    redirectTo: '/auth',
+    pathMatch: 'full'
   },
   {
-      path: 'auth',
-      loadChildren: './auth/auth.module#AuthModule'
+    path: 'auth',
+    loadChildren: './auth/auth.module#AuthModule'
   },
+  {
+    path: 'challenges',
+    loadChildren: './challenges/challenges.module#ChallengesModule'
+  }
 ];
