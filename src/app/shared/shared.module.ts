cd8e925e80e2a20cba9e3c11215e66c15daa6150
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-09-03 07:38:10 +0300

    modify sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { ActionBarComponent } from './ui/action-bar/action-bar.component';
 
 @NgModule({
-  imports: [],
-  declarations: [],
-  exports: []
+  imports: [RouterModule],
+  declarations: [ActionBarComponent],
+  exports: [ActionBarComponent]
 })
 export class SharedModule {}
